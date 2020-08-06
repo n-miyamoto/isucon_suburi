62c2f9667cdeda35480507e8694d3d4556225785
n-miyamoto <scotsdowneroad@gmail.com>
2020-08-06 22:50:37 +0900

    fix seller

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -346,11 +346,17 @@ module Isucari
       end
 
       item_details = items.map do |item|
-        seller = get_user_simple_by_id(item['seller_id'])
-        if seller.nil?
+        # seller = get_user_simple_by_id(item['seller_id'])
+        # if seller.nil?
+        if item['san'].nil?
           db.query('ROLLBACK')
           halt_with_error 404, 'seller not found'
         end
+        seller = {
+          'id' => items['sid'],
+          'account_name' => items['san'],
+          'num_sell_items' => user['ssi']
+        }
 
         category = get_category_by_id(item['category_id'])
         if category.nil?
