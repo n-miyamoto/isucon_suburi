e3b3ffcba36c87550705bf38dccf6062d3f86b6c
n-miyamoto <scotsdowneroad@gmail.com>
2020-08-07 20:34:34 +0900

    fix query

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -259,7 +259,7 @@ module Isucari
       items = if item_id > 0 && created_at > 0
         #db.xquery("SELECT * FROM `items` WHERE `status` IN (?, ?) AND category_id IN (?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", ITEM_STATUS_ON_SALE, ITEM_STATUS_SOLD_OUT, category_ids, Time.at(created_at), Time.at(created_at), item_id)
         db.xquery("SELECT i.*,
-                           s.`id` sid, s.`account_name` san, s.`num_sell_items` ssi,
+                           s.`id` sid, s.`account_name` san, s.`num_sell_items` ssi
                    FROM `items` i LEFT JOIN `users` s ON i.`seller_id` = s.`id` 
                    WHERE i.`status` IN (?, ?) AND i.`category_id` IN (?) AND (i.`created_at` < ?  OR (i.`created_at` <= ? AND i.`id` < ?)) 
                    ORDER BY i.`created_at` DESC, i.`id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", 
@@ -267,12 +267,11 @@ module Isucari
      else
         #db.xquery("SELECT * FROM `items` WHERE `status` IN (?,?) AND category_id IN (?) ORDER BY `created_at` DESC, `id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", ITEM_STATUS_ON_SALE, ITEM_STATUS_SOLD_OUT, category_ids)
         db.xquery("SELECT i.*,
-                           s.`id` sid, s.`account_name` san, s.`num_sell_items` ssi,
+                           s.`id` sid, s.`account_name` san, s.`num_sell_items` ssi
                    FROM `items` i LEFT JOIN `users` s ON i.`seller_id` = s.`id` 
                    WHERE i.`status` IN (?,?) AND i.`category_id` IN (?) 
                    ORDER BY i.`created_at` DESC, i.`id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", 
                    ITEM_STATUS_ON_SALE, ITEM_STATUS_SOLD_OUT, category_ids)
-
       end
 
       item_simples = items.map do |item|
